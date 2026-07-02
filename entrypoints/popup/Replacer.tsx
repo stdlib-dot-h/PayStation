acb43d2f40cbba8726aaa070012d242545470440
stdlib-dot-h <caniuseityet@gmail.com>
2026-07-01 21:02:29 -0700

    payment processing added
    Removed racially insensitive title and description from button.

--- entrypoints/popup/Replacer.tsx
+++ entrypoints/popup/Replacer.tsx
@@ -18,11 +18,6 @@ const Replacer = ({ from, to, enabled }: ReplacerType) => {
         <button
             class="border-2 p-2 gap-1 rounded-lg text-sm font-semibold items-baseline text-left shadow-xs flex flex-col border-gray-200 hover:bg-gray-200 cursor-pointer"
             onClick={toggleEnabled}
-            title={
-                to === "Actually Indians" ?
-                    "I am not a racist. Don't Be A Racist"
-                :   undefined
-            }
         >
             <label class="flex items-center gap-1">
                 <span class="text-xs opacity-80">
@@ -38,15 +33,6 @@ const Replacer = ({ from, to, enabled }: ReplacerType) => {
             </label>
             <span>
                 {from} -&gt; {to}
-            </span>
-            <span class="opacity-70 text-xs max-w-md">
-                {to === "Actually Indians" && (
-                    <>
-                        Actually Indians is a reference to when builder.ai was
-                        overworking indians in the name of AI and it was backed
-                        by Microslop
-                    </>
-                )}
             </span>
         </button>
     );
