e8bfce4c3a7415b639af733787758506f3f8f6ba
stdlib-dot-h <caniuseityet@gmail.com>
2026-07-01 20:59:49 -0700

    they must pay not play

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,23 +4,24 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     zip: {
-        name: "microslop",
+        name: "paystation",
         excludeSources: ["debub.log", "bun.lock"],
     },
     manifest: {
         version: "0.0.5",
-        name: "Microslop",
-        short_name: "Microslop",
-        description: "An extension that converts Microsoft to Microslop",
+        name: "PayStation",
+        short_name: "PayStation",
+        description: "An extension that converts PlayStation to PayStation",
         host_permissions: ["*://*/*"],
         browser_specific_settings: {
-            gecko: {
-                id: "microslop@4o4",
-                strict_min_version: "109.0",
-                ["data_collection_permissions" as any]: {
-                    required: ["none"],
-                },
-            },
+            //idk sry guys
+            //gecko: {
+                //id: "microslop@4o4",
+                //strict_min_version: "109.0",
+               // ["data_collection_permissions" as any]: {
+                //    required: ["none"],
+                //},
+           // },
         },
         permissions: ["storage"],
     },
