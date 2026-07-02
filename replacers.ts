f103329f7bffbbb5b4d10a4403408ae8e3775aee
stdlib-dot-h <caniuseityet@gmail.com>
2026-07-01 20:57:21 -0700

    make them teh paystation :)
    Removed several replacers and added new ones for PlayStation.

--- replacers.ts
+++ replacers.ts
@@ -7,75 +7,15 @@ export type ReplacerType = {
 
 export const replacers = [
     {
-        from: "Microsoft",
-        to: "Microslop",
+        from: "PlayStation",
+        to: "PayStation",
         ignorePrefix: ["@", "#"],
         enabled: true,
     },
     {
-        from: "Satya Nadella",
-        to: "Slopya Nuttela",
+        from: "Play Has No Limits",
+        to: "Pay Has No Limits",
         ignorePrefix: ["@", "#"],
         enabled: true,
-    },
-    {
-        from: "Satya Narayana Nadella",
-        to: "Slopya Narayana Nuttela",
-        ignorePrefix: ["@", "#"],
-        enabled: true,
-    },
-    {
-        from: "Artificial Intelligence",
-        to: "Actually Indians",
-        ignorePrefix: ["@", "#"],
-        enabled: true,
-    },
-
-    // extras
-
-    {
-        from: "Copilot",
-        to: "Slopilot",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "Windows",
-        to: "Bindoj",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "Xbox",
-        to: "GreedYbox",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "Game Pass",
-        to: "Gay Pass",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "OneDrive",
-        to: "CloudTumor",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "GitHub",
-        to: "ShitHub",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "Azure",
-        to: "Assure",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "Generative AI",
-        to: "Degenerative AI",
-        ignorePrefix: ["@", "#"],
-    },
-    {
-        from: "GenAI",
-        to: "DegenAI",
-        ignorePrefix: ["@", "#"],
     },
 ] satisfies ReplacerType[];
